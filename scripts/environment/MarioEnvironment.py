Fixed Issues with Action Selection and Frame Inputs, Implemented Softmax

scripts/environment/MarioEnvironment.py
@@ -13,30 +13,28 @@ from nn.setup import AgentParameters
 
 # Functions
 def create_mario_environment(environment_name='SuperMarioBros-v0'):
-    env = gym_super_mario_bros.make(environment_name)
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    env = ConcatObs(env=env, k=4)
-    return env
+    environment = gym_super_mario_bros.make(environment_name)
+    environment = JoypadSpace(environment, SIMPLE_MOVEMENT)
+    environment = ConcatObs(env=environment, k=4)
+    return environment
 
 
 def test_mario_model(model):
     done = False
     state = env.reset()
-    frames = np.zeros(shape=(32, 4, model.agent_parameters['downsample_w'], model.agent_parameters['downsample_h']))
+    frames = np.zeros(shape=(1, model.agent_parameters['n_frames'], model.agent_parameters['downsample_w'], model.agent_parameters['downsample_h']))
     for episode in range(20000):
-        time.sleep(0.01)
         env.render()
         observation = torch.from_numpy(state.copy()).float()
-        processed_state = model.preprocess.forward(observation[episode % 4, ])
-        frames[episode % 4, 0] = processed_state
+        processed_state = model.preprocess.forward(observation[episode % model.agent_parameters['n_frames'], ])
+        frames[0, episode % model.agent_parameters['n_frames']] = processed_state
 
         data = torch.from_numpy(frames).to(model.device)
-        action = model.forward(data)
-        actions = np.array(action.cpu().detach().numpy())
+        action_probability = torch.nn.functional.softmax(model.forward(data).mul(model.agent_parameters['action_conf']), dim=1)
+        m = torch.distributions.Categorical(action_probability)
+        action = m.sample().item()
 
-        action = get_weighted_action(actions) % 7
-
-        for _ in range(4):
+        for _ in range(model.agent_parameters['n_repeat']):
             obs, reward, done, _ = env.step(action)
             if done:
                 break
@@ -48,7 +46,9 @@ if __name__ == "__main__":
     # Environment Setup
     env = create_mario_environment()
 
-    model_name = '07-24-2023_16-06_NN=RNNIndividual_POPSIZE=20_GEN=80_PMUTATION_0.6_PCROSSOVER_0.5'
+    model_name = '-08-04-2023_18-52_NN=CNNIndividual_POPSIZE=30_GEN=50_PMUTATION_0.33_PCROSSOVER_0.5_I=0_SCORE=1450.0'
+
+    print(env.action_space)
 
     # Create and load a Model
     model = CNN(AgentParameters.MarioCudaAgent().agent_parameters)

scripts/environment/util/EnviromentUtil.py
@@ -6,15 +6,18 @@ import numpy as np
 from gym import spaces
 
 
+# replace with softmax
 def get_weighted_action(action_probabilities):
     action_probabilities = action_probabilities.flatten()
     weighted_indices = []
+    random_index = 0
     for i, val in enumerate(action_probabilities):
         weight = val ** 2  # Adjust the weighting here (e.g., val ** 2, val ** 3)
         weighted_indices.extend([i] * int(weight * 100))  # Scale the weights as desired
 
-    # Randomly select an index from the weighted indices list
-    random_index = random.choice(weighted_indices)
+    if len(weighted_indices) != 0:
+        # Randomly select an index from the weighted indices list
+        random_index = random.choice(weighted_indices)
     return random_index
 
 
@@ -41,5 +44,3 @@ class ConcatObs(gym.Wrapper):
 
     def _get_ob(self):
         return np.array(self.frames)
-
-

scripts/ga/components/Individuals.py
@@ -52,7 +52,7 @@ class CNNIndividual(Individual):
         old_fitness = None
         agent_parameters = self.nn.agent_parameters
         n_episodes = agent_parameters['n_episodes']
-        frames = np.zeros(shape=(32, 4, agent_parameters['downsample_w'], agent_parameters['downsample_h']))
+        frames = np.zeros(shape=(1, agent_parameters['n_frames'], agent_parameters['downsample_w'], agent_parameters['downsample_h']))
         deadrun = n_episodes / 2
         checkfit = int(round(n_episodes / 3))
         for episode in range(n_episodes):
@@ -61,15 +61,19 @@ class CNNIndividual(Individual):
             if render:
                 env.render()
             obs = torch.from_numpy(state.copy()).float()
-            processed_state = self.nn.preprocess.forward(obs[episode % 4,])
-            frames[episode % 4, 0] = processed_state
-            data = torch.from_numpy(frames).to(self.nn.device)
-            action = self.nn.forward(data)
-            actions = np.array(action.cpu().detach().numpy())
 
-            action = get_weighted_action(actions) % 7
+            # Update frames
+            processed_state = self.nn.preprocess.forward(obs[episode % agent_parameters['n_frames'], ])
+            frames[0, episode % agent_parameters['n_frames']] = processed_state
+            data = torch.from_numpy(frames).to(self.nn.device)
+
+            # Determine the action
+            action_probability = torch.nn.functional.softmax(self.nn.forward(data).mul(agent_parameters['action_conf']), dim=1)
+            m = torch.distributions.Categorical(action_probability)
+            action = m.sample().item()
+
             # Repeat the action for a few frames
-            for _ in range(4):
+            for _ in range(agent_parameters['n_repeat']):
                 obs, reward, done, _ = env.step(action)
                 fitness += reward
                 if done:
@@ -86,5 +90,47 @@ class CNNIndividual(Individual):
 
             if done:
                 break
+
+        return fitness, self.nn.get_weights_biases()
+
+
+# Convolutional Neural Network Individual
+class ReinforcementCNNIndividual(Individual):
+    def __init__(self, parameters):
+        super().__init__(parameters)
+
+    def get_model(self, parameters) -> NeuralNetwork:
+        return CNN(parameters)
+
+    def get(self, parameter):
+        return self.nn.agent_parameters[parameter]
+
+    # This is where actions the agent take are calculated, fitness is modified here.
+    def run_single(self, env, logger, render=False) -> Tuple[float, np.array]:
+        fitness = 0.0
+        discount_factor = 0.95
+        eps = 0.5
+        eps_decay_factor = 0.999
+        model = self.nn
+        state = env.reset()
+        eps *= eps_decay_factor
+        done = False
+        while not done:
+            if np.random.random() < eps:
+                action = np.random.randint(0, env.action_space.n)
+            else:
+                action = np.argmax(
+                    model.forward(np.identity(env.observation_space.n)[state:state + 1]))
+
+            new_state, reward, done, _ = env.step(action)
+            target = reward + discount_factor * np.max(
+                    model.forward(
+                        np.identity(env.observation_space.shape)[new_state:new_state + 1]))
+            target_vector = model.forward(
+                np.identity(env.observation_space.shape)[state:state + 1])[0]
+            target_vector[action] = target
+            state = new_state
+
+            fitness += target
 
         return fitness, self.nn.get_weights_biases()

scripts/nn/agents/CNN.py
@@ -27,12 +27,12 @@ class CNN(nn.Module, NeuralNetwork):
         self.conv1 = nn.Conv2d(in_channels=input_size, out_channels=hidden_size, kernel_size=8, stride=4).to(
             self.device)
         self.conv2 = nn.Conv2d(in_channels=hidden_size, out_channels=8, kernel_size=2, stride=2).to(self.device)
-        self.fc1 = nn.Linear(in_features=800, out_features=hidden_size).to(self.device)
+        self.fc1 = nn.Linear(in_features=1800, out_features=hidden_size).to(self.device)
         self.fc2 = nn.Linear(in_features=hidden_size, out_features=output_size).to(self.device)
 
         self.relu = nn.ReLU().to(self.device)
 
-    def forward(self, raw_input: Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, raw_input: Tensor) -> torch.Tensor:
         f_input = self.relu(self.conv1(raw_input.float()))
         f_input = self.relu(self.conv2(f_input))
         f_input = f_input.view(f_input.size(0), -1)

scripts/nn/preprocess/preproc.py
@@ -48,6 +48,6 @@ class Preproc(nn.Module):
         #         value = x[i,j]
         #         value = int(value.item() * 255)
         #         newImage.putpixel((j,i),(value,value,value))
-        # newImage.save("../../../image_snapshot/snapshot" + str(now.strftime("%H-%M-%S")) + ".png", 'PNG')
+        # newImage.save("../../image_snapshot/snapshot" + str(now.strftime("%H-%M-%S")) + ".png", 'PNG')
 
         return x